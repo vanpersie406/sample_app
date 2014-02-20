08094201edb7793d9346261bd5a68b6e3b4d1b20
vanpersie406 <arshavin406@hotmail.fr>
2014-02-20 18:11:36 +0100

    end of 5th chap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+ def full_title(page_title)
+    full_title = "Ruby on Rails Tutorial App"
+    page_title.present? ? (full_title += " | #{page_title}") : full_title
+  end
+ 
 end
