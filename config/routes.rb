7f71ad56780a7cc2b5469c2ad42a489a36b2832e
vanpersie406 <arshavin406@hotmail.fr>
2014-02-22 17:23:45 +0100

    chap7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
- 
+  resources :users
   get "users/new"
-   root  'static_pages#home'
+  root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
